2bbe503d402e2f7fae71855d850d6550c2a5ca4b
Sinhala Gedera Malitha Ridhmal Rathnathilaka <malitha.uog04@edu.lnbti.lk>
2026-01-01 12:21:28 +0530

    Fix registration/profile to handle installations without 'name' column: map name -> first_name/last_name, generate username, avoid SQL errors in tests

--- backend/app/Models/User.php
+++ backend/app/Models/User.php
@@ -20,6 +20,7 @@ class User extends Authenticatable
      * @var list<string>
      */
     protected $fillable = [
+        'name',
         'first_name',
         'last_name',
         'username',
